606f8b7ea98c7ae38040ca9e7ce11998082b038c
Kenta Kodama <kentakodama@gmail.com>
2018-01-27 16:09:26 +0700

    rendered map poutline

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+  
+ // const phrase = Document.querySelector('#phrase')
+  
+  // phrase.textContent = 'fuck you!'
+ // const words = document.createElement('p');
+ // 
+ // words.innerHTML = 'fuck you kenta!'
+ // 
+
+   const phrase = document.querySelector("p")
+   console.log(phrase);
+   phrase.textContent = "New text!"
+
+
+
+ 
